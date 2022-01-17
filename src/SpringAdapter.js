16a19ee4925a602dca17796298c7358dc23f9ece
Sebastian Gedigk <sebastian.gedigk@haw-hamburg.de>
2022-01-17 12:46:31 +0100

    Jwt nutzen beim create

--- src/SpringAdapter.js
+++ src/SpringAdapter.js
@@ -1,9 +1,14 @@
 /** */
 export default class SpringAdapter {
   /** */
-  constructor(canvasId, endpointUrl) {
+  constructor(canvasId, endpointUrl, tokenrefreshUrl) {
     this.canvasId = canvasId;
     this.endpointUrl = endpointUrl;
+    this.tokenrefreshUrl = tokenrefreshUrl;
+    this.token = sessionStorage.getItem('auth-token');
+    this.refreshtoken = sessionStorage.getItem('auth-refreshtoken');
+    this.tokenwasExpired = false;
+
   }
 
   /** */
@@ -13,6 +18,14 @@ export default class SpringAdapter {
 
   /** */
   async create(annotation) {
+    let myHeaders = new Headers();
+    if (this.token) {
+      const bearer = 'Bearer ' + this.token;
+      myHeaders.append('Authorization', bearer);
+    }
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
+
     return fetch(this.endpointUrl, {
       body: JSON.stringify({
         annotation: {
@@ -21,18 +34,42 @@ export default class SpringAdapter {
           uuid: annotation.id,
         },
       }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
+      headers: myHeaders,
+
       method: 'POST',
+      withCredentials: true,
+
     })
-      .then((response) => this.all())
+      .then(response => {
+        console.log(response.status); // Will show you the status
+        if (!response.ok) {
+          if (response.status === 401) {
+            this.tokenwasExpired = true;
+            this.renewToken();
+            if (this.tokenwasExpired) {
+              alert('Login abgelaufen! Bitte ausloggen und neu einloggen.');
+              this.tokenwasExpired = false;
+              sessionStorage.removeItem('auth-token');
+              sessionStorage.removeItem('auth-refreshtoken');
+            } else {
+              this.create(annotation);
+            }
+          }
+        }
+        this.all();
+      })
       .catch(() => this.all());
   }
 
   /** */
   async update(annotation) {
+    let myHeaders = new Headers();
+    if (this.token) {
+      const bearer = 'Bearer ' + this.token;
+      myHeaders.append('Authorization', bearer);
+    }
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
     return fetch(this.endpointUrl, {
       body: JSON.stringify({
         annotation: {
@@ -41,11 +78,9 @@ export default class SpringAdapter {
           uuid: annotation.id,
         },
       }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
+      headers: myHeaders,
       method: 'PATCH',
+      withCredentials: true,
     })
       .then((response) => this.all())
       .catch(() => this.all());
@@ -53,6 +88,13 @@ export default class SpringAdapter {
 
   /** */
   async delete(annoId) {
+    let myHeaders = new Headers();
+    if (this.token) {
+      const bearer = 'Bearer ' + this.token;
+      myHeaders.append('Authorization', bearer);
+    }
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
     return fetch(this.endpointUrl, {
       body: JSON.stringify({
         annotation: {
@@ -60,11 +102,9 @@ export default class SpringAdapter {
           uuid: annoId,
         },
       }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
+      headers: myHeaders,
       method: 'DELETE',
+      withCredentials: true,
     })
       .then((response) => this.all())
       .catch(() => this.all());
@@ -83,5 +123,38 @@ export default class SpringAdapter {
   /** */
   async all() {
     return (await fetch(this.annotationPageId)).json();
+  }
+
+  /** */
+  renewToken() {
+    let myHeaders = new Headers();
+    if (this.token) {
+      const bearer = 'Bearer ' + this.refreshtoken;
+      myHeaders.append('Authorization', bearer);
+    }
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
+    fetch(this.tokenrefreshUrl, {
+      headers: myHeaders,
+      method: 'GET',
+      withCredentials: true,
+    }).then(response => {
+      response.json()
+      console.log(response);
+    }
+    )
+      .then(data => {
+        console.log(data);
+        sessionStorage.removeItem('auth-token');
+        sessionStorage.removeItem('auth-refreshtoken');
+        sessionStorage.setItem('auth-token', data.access_token);
+        sessionStorage.setItem('auth-refreshtoken', data.refresh_token);
+        this.tokenwasExpired = false;
+      })
+      .catch(() => 
+      {
+        this.all();
+        console.log('Catch reingegangen');
+      })
   }
 }
